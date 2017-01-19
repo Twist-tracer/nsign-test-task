0b8e913f85105f895ccfea00de3c8877137c64fa
Twist tracer <twist.tracer@gmail.com>
2017-01-19 09:26:23 +0300

    added check for empty data

--- views/site/index.php
+++ views/site/index.php
@@ -15,39 +15,40 @@ $this->title = 'Меню';
     </div>
 
     <div class="body-content">
-        <table class="table table-bordered table-responsive">
-            <thead>
-                <th>
-                    Название блюда
-                </th>
-                <th>
-                    Ингредиенты
-                </th>
-            </thead>
-            <tbody>
-                <?php foreach($dishes as $dish) {?>
+        <?php if(!empty($dishes)) { ?>
+            <table class="table table-bordered table-responsive">
+                <thead>
+                    <th>
+                        Название блюда
+                    </th>
+                    <th>
+                        Ингредиенты
+                    </th>
+                </thead>
+                <tbody>
+                    <?php foreach($dishes as $dish) {?>
+                        <tr>
+                            <td><?=$dish->name?></td>
+                            <td>
+                                <?php if(!empty($dish->ingredients)) {?>
+                                    <?=implode(', ', $dish->ingredients)?>
+                                <?php } else {?>
+                                    <?='На данный момент интгридентов к этому блюду нет.'?>
+                                <?php }?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
 
-                    <tr>
-                        <td><?=$dish->name?></td>
-                        <td>
-                            <?php if(!empty($dish->ingredients)) {?>
-                                <?=implode(', ', $dish->ingredients)?>
-                            <?php } else {?>
-                                <?='На данный момент интгридентов к этому блюду нет.'?>
-                            <?php }?>
-                        </td>
-                    </tr>
-
-                <?php } ?>
-            </tbody>
-
-        </table>
-
-        <div class="pafination">
-            <?= LinkPager::widget([
-                'pagination' => $pagination,
-            ])?>
-        </div>
+            <div class="pafination">
+                <?= LinkPager::widget([
+                    'pagination' => $pagination,
+                ])?>
+            </div>
+        <?php } else {?>
+            <h2>В нашем меню пока нет блюд, но не переживайте, они скоро здесь появятся.</h2>
+        <?php }?>
 
     </div>
 </div>
